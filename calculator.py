d8e35e373d1a8ece65f7ff48e152a8eb093fc983
Kyle <kyle216@gmail.com>
2021-04-29 16:39:42 -0700

    Calculator program with unittest
    Created all files

---
+++ calculator.py
@@ -0,0 +1,14 @@
+def add (a,b):
+    return a + b
+
+def sub (a,b):
+    return a - b
+
+def multi (a,b):
+    return a * b
+
+def div (a,b):
+    if (a != 0):
+        return a / b
+    else:
+        return 0
---
+++ test_calculator.py
@@ -0,0 +1,23 @@
+import unittest
+import calculator
+
+class testCaseAdd(unittest.TestCase):
+    def test_add(self):
+        self.assertEqual(calculator.add(10,5), 15)
+        self.assertEqual(calculator.add(10,5), 216)
+
+    def test_sub(self):
+        self.assertEqual(calculator.sub(10,5), 5)
+        self.assertEqual(calculator.sub(10,5), 432)
+
+    def test_multi(self):
+        self.assertEqual(calculator.multi(10,5), 50)
+        self.assertEqual(calculator.multi(10,5), 864)
+
+    def test_div(self):
+        self.assertEqual(calculator.div(10,5), 2)
+        self.assertEqual(calculator.div(0,5), 0)
+
+
+if __name__ == '__main__':
+    unittest.main()
